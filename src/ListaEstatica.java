831b943dbbf4fe63f20647058152ea5800436ff6
Alysson Milanez <alyssonfilgueira@gmail.com>
2020-11-13 00:22:51 -0300

    Criando a estrutura

--- src/ListaEstatica.java
+++ src/ListaEstatica.java
@@ -3,5 +3,10 @@ public class ListaEstatica<T> {
 	
 	private T[] elementos;
 	private int quantidade;
+	
+	public ListaEstatica(int tamanho){
+		this.elementos = (T[]) new Object[tamanho];
+		this.quantidade = 0;		
+	}
 
 }

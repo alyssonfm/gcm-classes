fe1e0b51cc99d2202a6cf8cb8b418c16f2d0d85f
Alysson Milanez <alyssonfilgueira@gmail.com>
2020-11-17 09:26:12 -0300

    Adicionando o inserir no Inicio

--- src/ListaEstatica.java
+++ src/ListaEstatica.java
@@ -1,4 +1,3 @@
-
 public class ListaEstatica<T> {
 	
 	private T[] elementos;
@@ -13,7 +12,23 @@ public class ListaEstatica<T> {
 		return this.quantidade == 0;
 	}
 	
+	public boolean isCheia() {
+		return this.quantidade == this.elementos.length;
+	}
+	
 	public int getQuantidade() {
 		return this.quantidade;
+	}
+	
+	
+	public void inserirInicio(T novoNo) {
+		if(this.isCheia()) System.out.println("Lista cheia, naoh eh possivel inserir");
+		else {
+			for(int i = this.quantidade; i > 0; --i) {
+				this.elementos[i] = this.elementos[i-1];
+			}
+			this.elementos[0] = novoNo;
+			++this.quantidade;
+		}
 	}
 }

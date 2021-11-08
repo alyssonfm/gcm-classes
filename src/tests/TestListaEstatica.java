1ae1847176bf35ff660b30b1d8270ba2cf211f52
Alysson Milanez <alyssonfilgueira@gmail.com>
2021-11-08 08:43:00 -0300

    Atualizacoes no pom

--- src/tests/TestListaEstatica.java
+++ src/tests/TestListaEstatica.java
@@ -8,50 +8,50 @@ import code.ListaEstatica;
 
 public class TestListaEstatica {
 
-	@Test
-	void testConstructor() {
-		ListaEstatica<String> l = new ListaEstatica<String>(10);
-		assertTrue(l.getQuantidade() == 0);
-		assertTrue(l.isVazia());
-		assertFalse(l.isCheia());
+    @Test
+    public void testConstructor() {
+	ListaEstatica<String> l = new ListaEstatica<String>(10);
+	assertTrue(l.getQuantidade() == 0);
+	assertTrue(l.isVazia());
+	assertFalse(l.isCheia());
+    }
+
+    @Test
+    public void testGetQuantidade() {
+	ListaEstatica<String> l = new ListaEstatica<String>(10);
+	assertTrue(l.getQuantidade() == 0);
+	try {
+	    l.inserirInicio("Alysson");
+	} catch (ListaCheiaException e) {
+	    e.printStackTrace();
 	}
-	
-	@Test
-	void testGetQuantidade() {
-		ListaEstatica<String> l = new ListaEstatica<String>(10);
-		assertTrue(l.getQuantidade() == 0);
-		try {
-			l.inserirInicio("Alysson");
-		} catch (ListaCheiaException e) {
-			e.printStackTrace();
-		}
-		assertEquals(l.getQuantidade(), 1);
+	assertEquals(l.getQuantidade(), 1);
+    }
+
+    @Test
+    public void testIsVazia() {
+	ListaEstatica<String> l = new ListaEstatica<String>(10);
+	assertTrue(l.isVazia());
+	try {
+	    l.inserirFim("Alysson");
+	    assertFalse(l.isVazia());
+
+	} catch (ListaCheiaException e) {
+	    e.printStackTrace();
 	}
-	
-	@Test
-	void testIsVazia() {
-		ListaEstatica<String> l = new ListaEstatica<String>(10);
-		assertTrue(l.isVazia());
-		try {
-		l.inserirFim("Alysson");
-		assertFalse(l.isVazia());
-		
-	    } catch (ListaCheiaException e) {
-		e.printStackTrace();
-	    }
+    }
+
+    @Test
+    public void testInserirFim() {
+	ListaEstatica<String> l = new ListaEstatica<String>(10);
+	try {
+	    l.inserirFim("Alysson");
+	    assertTrue(l.pesquisar("Alysson") != -1);
+
+	} catch (ListaCheiaException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
-	
-	@Test
-	void testInserirFim() {
-	    ListaEstatica<String> l = new ListaEstatica<String>(10);
-	    try {
-		l.inserirFim("Alysson");
-		assertTrue(l.pesquisar("Alysson") != -1);
-		
-	    } catch (ListaCheiaException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    }
-	}
+    }
 
 }
